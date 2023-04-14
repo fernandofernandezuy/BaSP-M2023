cd37cd4ce59b6db18033904448b3a1aa9c93fcff
Fernando Fernández <83631907+fernandofernandezuy@users.noreply.github.com>
2023-04-14 18:37:58 -0300

    BaSP-W05-2023: Completed Exercise 1

---
+++ Week-05/exercises/arrays.js
@@ -0,0 +1,8 @@
+let num1 = 17;
+let num2 = 10;
+let sum = num1 + num2;
+
+let name = 'Fernando';
+let surName = 'Fernandez';
+let fullName = name + ' ' + surName;
+let fullNameLenght = name.length + surName.length;
---
+++ Week-05/exercises/variables-and-operators.js
@@ -0,0 +1,25 @@
+console.log('--EXERCISE 1: VARIABLES AND OPERATORS');
+
+// a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+
+console.log('-Exercise 1.a:');
+let num1 = 17;
+let num2 = 10;
+let sum = num1 + num2;
+console.log(sum);
+
+// b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+console.log('-Exercise 1.b:');
+let street = 'Av. Italia';
+let number = '3587';
+let address = street + ' ' + number;
+console.log(address);
+
+// c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+
+console.log('-Exercise 1.c:');
+let name = 'Fernando';
+let surName = 'Fernandez';
+let fullNameLenght = name.length + surName.length;
+console.log(fullNameLenght);
